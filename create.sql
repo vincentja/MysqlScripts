ec8c642eb801fad0b7255733782c381c31d5f728
vincentja <linuxjia@gmail.com>
2013-12-21 11:42:03 +0800

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,2 +1,2 @@
 create database wn_tv;
-create table hntv_hash(id BIGINT  NOT NULL auto_increment,hash char(64) ,PRIMARY KEY (id)) ENGINE = MEMORY;
+create table hashs(id BIGINT  NOT NULL auto_increment,hash char(64) ,PRIMARY KEY (id)) ENGINE = MEMORY;
